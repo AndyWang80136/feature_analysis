a959d2a1e442e2e199311735c850866ef51eb087
AndyWang(Wang Chun-Hao) <andy80136@gmail.com>
2023-08-07 10:06:47 +0800

    feat: add prcess test functions

---
+++ tests/test_process.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+import pytest
+
+from feature_analysis.process import train_process
+
+
+def test_train_process():
+    result = train_process(data={'name': 'RandomDataset'},
+                           model={},
+                           features={},
+                           lr=0.01,
+                           optimizer='SGD',
+                           epochs=1)
+    assert isinstance(result, dict)
+    assert 'best_metric' in result
+    assert 'best_value' in result
+    assert 'other_metrics' in result
